83b79a149462a17901095f6e3ec76d96697e131a
Ritsuya Saito <ritsuya.basketball@gmail.com>
2019-07-31 21:13:24 +0900

    Initial Commit

---
+++ qiitaAPI2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qiitaAPI2
+//
+//  Created by 齋藤律哉 on 2019/07/31.
+//  Copyright © 2019 ritsuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
